d40fd5c4a59ba57e6e16295451afdde4029104a4
it-al-amin <61705923+it-al-amin@users.noreply.github.com>
2020-03-08 11:54:10 +0600

    alamin commit

---
+++ register.java
@@ -0,0 +1,18 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Md Al Amin on 3/8/2020.
+ */
+public class register extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
